18cc636786b2110ac2bf532053edfa0847138521
David Holtz <david.richard.holtz@gmail.com>
2020-05-02 00:50:52 -0400

    🤯 simplify and add serde

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,13 +1,18 @@
-use breakouts::{split_vec_to_index, HasCool};
+use breakouts::{split_vec_to_index, string_to_static_str, HasCool};
 use rand::distributions::{Distribution, Uniform};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::time::Instant;
 
-#[derive(Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Person {
     pub name: String,
     pub attrs: PersonAttrs,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct PersonAttrs {
     pub is_cool: bool,
     pub age: usize,
@@ -31,46 +36,61 @@ pub fn make_me() -> Person {
 }
 
 impl HasCool for Person {
-    fn bool_val(&self) -> bool {
-        self.attrs.is_cool
-    }
-    fn number_val(&self) -> usize {
-        self.attrs.age
-    }
-    fn string_val(&self) -> String {
-        self.attrs.fav_color.to_string()
+    fn attributes(&self) -> Vec<(&str, &str)> {
+        return [
+            ("is_cool", &string_to_static_str(self.attrs.is_cool)[..]),
+            ("name", &self.name[..]),
+            ("age", &string_to_static_str(self.attrs.age)[..]),
+            ("fav_color", &self.attrs.fav_color[..]),
+        ]
+        .to_vec();
     }
 }
 
 fn main() {
+    // Make some dummy data - like 1M
     let mut vec = Vec::new();
-
     for _ in 0..1_000_000 {
+        // for _ in 0..50_000 {
         vec.push(make_me());
     }
 
+    // Index this data and time it
     let start = Instant::now();
     let res = split_vec_to_index::<Person>(&vec);
     let duration = start.elapsed();
     println!("Time elapsed while indexing: {:?}", duration);
 
+    // Now we wanna save it - as bytes to disk
     let start = Instant::now();
-    // let mut myresults: Vec<String> = Vec::new();
+    let encoded: Vec<u8> = bincode::serialize(&res).unwrap();
+    let mut file = File::create("test").unwrap();
+    file.write_all(&encoded[..]).unwrap();
+    let duration = start.elapsed();
+    println!("Time elapsed while serialize: {:?}", duration);
+
+    // Now we wanna read those bytes
+    let start = Instant::now();
+    let mut file = File::open("test").unwrap();
+    let mut encoded = Vec::new();
+    file.read_to_end(&mut encoded).expect("failed to read");
+    let res: HashMap<(&str, &str), Vec<usize>> = bincode::deserialize(&encoded[..]).unwrap();
+    let duration = start.elapsed();
+    println!("Time elapsed while deserialize: {:?}", duration);
+
+    // Last we want to check which of those people attributes meet some criteria
+    let start = Instant::now();
     for (k, _v) in res.iter() {
-        // things we want to lookup
-        if k.0 == "number_val" && k.1.clone().as_usize() >= 60 {
-            // something did match
-            // println!("number_val {:?}: {:?}", k, _v);
-            // myresults.push(format!("number_val {:?}: {:?}", k, _v))
+        if k.0 == "is_cool" && k.1 == "true" {
+            // println!("name::david {:?}: {:?}", k, _v); // print match and indexes
         }
-        if k.0 == "string_val" && k.1.clone().as_string() == String::from("Red") {
-            // something did match
-            // println!("string_val {:?}: {:?}", k, _v);
-            // myresults.push(format!("string_val {:?}: {:?}", k, _v))
+        if k.0 == "name" && k.1 == "David" {
+            // println!("name::david {:?}: {:?}", k, _v); // print match and indexes
+        }
+        if k.0 == "age" && k.1.to_string().parse::<usize>().unwrap() >= 60 {
+            // println!("age::sixty {:?}: {:?}", k, _v); // print match and indexes
         }
     }
     let duration = start.elapsed();
     println!("Time elapsed while grouping: {:?}", duration);
 }
-
-use std::time::Instant;
